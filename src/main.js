317021342b0133281a0f0b556982716bcd12b2fc
Eason <eason.koocover.com>
2018-07-26 21:09:10 +0700

    完成了首页的数据渲染

--- src/main.js
+++ src/main.js
@@ -6,12 +6,36 @@ import VueRouter from 'vue-router'
 import App from './App.vue'
 //引入css这个模块
 import'./assets/statics/site/css/style.css'
+//引入这个模块
+import ElementUI from 'element-ui';
+//引入这个css
+import 'element-ui/lib/theme-chalk/index.css';
+//引入懒加载这个模块
+import VueLazyload from 'vue-lazyload'
 
 // 引入路由
 import index from './components/index.vue';
+//引入路劲
+import path from 'path'
 
 // 路由中间件
 Vue.use(VueRouter)
+//路由中间件
+Vue.use(ElementUI);
+//使用中间件
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  error: path.join(__dirname, './assets/statics/img/01.gif'),
+  loading:path.join(__dirname, './assets/statics/img/01.gif'),
+  attempt: 1
+})
+// Vue.use(VueLazyload, {
+//   preLoad: 1.3,
+//   error: './assets/statics/img/01.gif',
+//   loading: './assets/statics/img/01.gif',
+//   attempt: 1
+// })
+
 
 // 定义路由规则
 const router = new VueRouter({
